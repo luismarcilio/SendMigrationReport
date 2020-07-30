eef3bd088e97fd0fcf492640625f3af4ae61a445
Luis Marcilio Braga de Oliveira <luismarcilio@yahoo.com.br>
2020-07-30 16:00:55 -0300

    using sample report on test

--- __tests__/unit/handlers/sendEmail.test.js
+++ __tests__/unit/handlers/sendEmail.test.js
@@ -1,16 +1,19 @@
-const { sendMigrationReport } = require("../../../index");
+const fs = require("fs");
 
 // This includes all tests for putItemHandler()
 describe("Test send email", function () {
   jest.setTimeout(30000);
   // This test invokes putItemHandler() and compare the result
   it("should send the email", async () => {
+    const body = fs.readFileSync("__tests__/unit/handlers/hourly_report.html");
+
     const event = {
       httpMethod: "POST",
-      body: "This is a test to send the email",
+      body,
     };
 
     // Invoke putItemHandler()
+    const { sendMigrationReport } = require("../../../index");
     const result = await sendMigrationReport(event);
     const expectedStatusCode = 200;
 
