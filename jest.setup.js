943e71cd145a105b16654cebd0d913fa62f53792
Luis Marcilio Braga de Oliveira <luismarcilio@yahoo.com.br>
2020-08-14 16:18:52 -0300

    removed login information

--- jest.setup.js
+++
@@ -1,8 +0,0 @@
-process.env.RECEIPIENTS = "deolivel@amdocs.com";
-process.env.HOST = "smtp-mail.outlook.com";
-process.env.USERNAME = "deolivel@amdocs.com";
-process.env.PASSWORD = "Iflscience04";
-console.log("process.env.RECEIPIENTS: ", process.env.RECEIPIENTS);
-console.log("process.env.HOST: ", process.env.HOST);
-console.log("process.env.USERNAME: ", process.env.USERNAME);
-console.log("process.env.PASSWORD: ", process.env.PASSWORD);
